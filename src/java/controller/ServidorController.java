38adcbeb820c54dfd7ff37799c12ad9ad4daf08f
MarilenaOshima <marilenaoshima@gmail.com>
2022-09-11 12:10:14 -0300

    importacao de servidores

--- src/java/controller/ServidorController.java
+++ src/java/controller/ServidorController.java
@@ -6,7 +6,9 @@
 package controller;
 
 import dao.ServidorDAO;
+import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJBException;
@@ -15,6 +17,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import model.Servidor;
 import org.primefaces.PrimeFaces;
+import org.primefaces.model.file.UploadedFile;
+import util.ReadExcel;
+import util.UploadFileToFile;
 import util.Util;
 
 /**
@@ -30,6 +35,9 @@ public class ServidorController implements Serializable {
 
     private Servidor servidor;
     private List<Servidor> servidores;
+    private UploadedFile file;
+    
+    private boolean loading = false;
 
     @PostConstruct
     public void fillServidorList() {
@@ -75,6 +83,36 @@ public class ServidorController implements Serializable {
         PrimeFaces.current().executeScript("PF('editServidorDialog').hide()");
         PrimeFaces.current().ajax().update("form:messages", "form:dt-servidores");
     }
+    
+        public void importarServidores() {
+        loading = true;
+        if (file != null) {
+            List<Servidor> planilha = new ArrayList<Servidor>();
+            List<Servidor> servidoresBanco = new ArrayList<Servidor>();
+
+            servidoresBanco = servidorDAO.buscarTodos();
+
+            File newFile = UploadFileToFile.uploadedFileToFileConverter(file);
+            planilha = util.ReadExcel.servidorReadExcel(newFile);
+            
+            for (Servidor s : planilha) {
+                if (servidoresBanco.contains(s)) {
+                    s.setSenha(servidoresBanco.get(servidoresBanco.indexOf(s)).getSenha());
+                    servidorDAO.update(s);
+                } else {
+                    servidorDAO.create(s);
+                }
+            }
+
+            PrimeFaces.current().executeScript("PF('importarServidor').hide()");
+            PrimeFaces.current().ajax().update("form:messages", "form:dt-servidores");
+            fillServidorList();
+            loading = false;;
+            util.Util.addMessageInformation("Servidor(es) Cadastrado(s)");
+        } else {
+            System.out.println("FILE NULL");
+        }
+    }
 
     public Servidor getServidor() {
         return servidor;
@@ -91,5 +129,25 @@ public class ServidorController implements Serializable {
     public void setServidores(List<Servidor> servidores) {
         this.servidores = servidores;
     }
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public boolean isLoading() {
+        return loading;
+    }
+
+    public void setLoading(boolean loading) {
+        this.loading = loading;
+    }
+    
+    
+    
+    
 
 }
---
+++ src/java/util/ReadExcel.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Servidor;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ *
+ * @author Marilena Oshima
+ */
+public class ReadExcel {
+    public static List<Servidor> servidorReadExcel(File fileImport) {
+        // cria uma lista de servidores
+        List<Servidor> servidores = new ArrayList<Servidor>();
+        // pra ler o arquivo
+        FileInputStream file = null;
+        
+        try {
+            // transforma o arquivo em file input stream
+            file = new FileInputStream(fileImport);
+            Workbook workbook = null;
+
+            if (fileImport.getName().toLowerCase().endsWith("xlsx")) {
+                workbook = new XSSFWorkbook(file);
+            } else if (fileImport.getName().toLowerCase().endsWith("xls")) {
+                workbook = new HSSFWorkbook(file);
+            }
+            //planilha
+            Sheet sheet = workbook.getSheetAt(0);
+            // iterar sobre as linhas da planilha
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.hasNext();
+
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                // instancia um servidor
+                Servidor s = new Servidor();
+
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getColumnIndex()) {
+                        case 2:
+                            s.setNome(cell.getStringCellValue());
+                            break;
+                        case 4:
+                            s.setTipo(cell.getStringCellValue());
+                            break;
+                        case 5:
+                            s.setEmail(cell.getStringCellValue());
+                            break;
+                        case 6:
+                            s.setProntuario(cell.getStringCellValue());
+                            s.setSenha(cell.getStringCellValue());
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
+                //System.out.println("SERVIDORRR S....." + s);
+                servidores.add(s);
+
+            }
+            file.close();
+            return servidores;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(util.ReadExcel.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (IOException ex) {
+            Logger.getLogger(util.ReadExcel.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
