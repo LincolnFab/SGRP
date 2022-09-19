8227430b5fbb43b74e725fb3b999099b59e45a93
MarilenaOshima <marilenaoshima@gmail.com>
2022-09-19 19:45:33 -0300

    importacao de estudantes

--- src/java/util/ReadExcel.java
+++ src/java/util/ReadExcel.java
@@ -14,14 +14,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Estudante;
 import model.SalaDeAula;
 import model.Servidor;
+import model.Turma;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 /**
  *
@@ -137,6 +140,78 @@ public class ReadExcel {
             file.close();
             System.out.println("salas" + salas);
             return salas;
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(util.ReadExcel.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } catch (IOException ex) {
+            Logger.getLogger(util.ReadExcel.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public static List<Estudante> estudanteExcelData(File fileImport, List<Turma> turmas) {
+        List<Estudante> estudantes = new ArrayList<Estudante>();
+        FileInputStream file = null;
+        
+        try {
+            file = new FileInputStream(fileImport);
+            Workbook workbook = null;
+
+            if (fileImport.getName().toLowerCase().endsWith("xlsx")) {
+                workbook = new XSSFWorkbook(file);
+            } else if (fileImport.getName().toLowerCase().endsWith("xls")) {
+                workbook = new HSSFWorkbook(file);
+            }
+            
+            Sheet sheet = workbook.getSheetAt(0);
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.hasNext();
+            Row row = rowIterator.next();
+            
+            while (rowIterator.hasNext()) {
+                row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                Estudante e = new Estudante();
+
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getColumnIndex()) {
+                        case 1:
+                            e.setProntuario(cell.getStringCellValue());
+                            e.setSenha(cell.getStringCellValue());
+                            break;
+                        case 2:
+                            e.setNome(cell.getStringCellValue());
+                            break;
+                        case 4:
+                            e.setEmailAluno(cell.getStringCellValue());
+                            break;
+                        case 5:
+                            e.setEmailPessoal(cell.getStringCellValue());
+                            break;
+                        case 6:
+                            e.setEmailResponsavel(cell.getStringCellValue());
+                            // se os e-mail forem - deixar em branco
+                            if (e.getEmailResponsavel().equalsIgnoreCase("-"))
+                                e.setEmailResponsavel("");
+                            break;
+                        case 7:
+                            Turma turma = new Turma();
+                            String codigoTurma = cell.getStringCellValue();
+                            for (Turma t : turmas) {
+                                if(t.getIdturma().equalsIgnoreCase(codigoTurma))
+                                    e.setTurmaIdturma(t);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                estudantes.add(e);
+            }
+            file.close();
+            return estudantes;
         } catch (FileNotFoundException ex) {
             Logger.getLogger(util.ReadExcel.class.getName()).log(Level.SEVERE, null, ex);
             return null;
